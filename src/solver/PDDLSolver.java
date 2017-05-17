87e6137913995092413ffc13eacca16eb8f9d343
Dieu Tran <tran.dieu@ulb.ac.be>
2017-05-17 22:52:05 +0200

    Improve solver: checking each level if output can cover goal

--- src/solver/PDDLSolver.java
+++ src/solver/PDDLSolver.java
@@ -1,7 +1,6 @@
 package solver;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
@@ -12,70 +11,32 @@ import org.apache.jena.ontology.OntModel;
 import main.Service;
 
 public class PDDLSolver {
-	
-	private static List extendState(List<String> state, OntModel m, List<String> possibleInputs){
-		List<String> newState = new ArrayList<String>(state);
-		String cla = "";
-		
-		for (String s : state){
-			OntClass oc = m.getOntClass(s);
-			if (oc.hasSuperClass()){
-				for (Iterator<OntClass> i = oc.listSuperClasses(); i.hasNext();){
-					cla = ((OntClass)i.next()).toString();
-					if (!newState.contains(cla))
-						newState.add(cla);
-				}
-			}
-			if (oc.hasSubClass()){
-				for (Iterator<OntClass> i = oc.listSubClasses(); i.hasNext();){
-					cla = ((OntClass)i.next()).toString();
-					if (!newState.contains(cla) && possibleInputs.contains(cla)){
-						newState.add(cla);
-						OntClass tmp = m.getOntClass(cla);
-						if (tmp.hasSuperClass()){
-							for (Iterator<OntClass> i2 = oc.listSuperClasses(); i2.hasNext();){
-								cla = ((OntClass)i2.next()).toString();
-								if (!newState.contains(cla))
-									newState.add(cla);
-							}
-						}
-					}
-				}
-			}
-		}
-		return newState;
-	}
 	public static String solve(List<String> initial, List<String> goal, 
 								List<Service> services, OntModel m){
 		int totalLevel = services.size();
 		Stack<Service> serviceStack = new Stack<Service>();
 		Stack<List<Service>> serviceQueue = new Stack<List<Service>>(); 
 		List<String> states = new ArrayList<String>(initial);
-//		List<String> possibleInput = new ArrayList<String>();
-//		
-//		for (Service service : services){
-//			for (String s : service.getServiceInput())
-//				if (!possibleInput.contains(s))
-//					possibleInput.add(s);
-//		}
-			
-//		states = extendState(initial, m, possibleInput);
-//		System.out.println("InitialState");
-//		for (String s : states)
-//			System.out.println(s);
 		int n = 0;
+		boolean flag = false;
 		do {
-			
 			List<Service> serviceList = new ArrayList<Service>();
 			if (serviceQueue.size() != totalLevel){
 				for (Service service : services){
 					if (serviceStack.contains(service))
 						continue;
-					if (isCovered2(service.getServiceInput(), states, m)){
+					if (isCovered(service.getServiceInput(), states, m)){
+						if (isCovered(goal, service.getServiceOutput(), m)){
+							flag = true;
+							serviceStack.push(service);
+							break;
+						}
 						serviceList.add(service);
 					}
 				}
 			}
+			if (flag)
+				break;
 			serviceQueue.push(serviceList);
 			
 			if (serviceList.size() != 0){
@@ -103,10 +64,10 @@ public class PDDLSolver {
 				}
 			}
 			
-		}while (!isCovered2(goal, states, m) && !serviceQueue.isEmpty());
+		}while (!isCovered(goal, states, m) && !serviceQueue.isEmpty());
 		
 		
-		if (isCovered(goal, states)){
+		if (flag || isCovered(goal, states, m)){
 			StringBuilder sb = new StringBuilder();
 			sb.append("Solution found: \n");
 			for (Service s : serviceStack)
@@ -117,48 +78,8 @@ public class PDDLSolver {
 			return "No solution found!";
 		}
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<String> in = Arrays.asList("book author".split(" "));
-		List<String> out = Arrays.asList("location".split(" "));
-		
-		Service service1 = new Service(Arrays.asList("book author".split(" ")), 
-				Arrays.asList("isbn".split(" ")),
-				"service 1");
-		
-		Service service2 = new Service(Arrays.asList("isbn".split(" ")), 
-				Arrays.asList("price".split(" ")),
-				"service 2");
-		
-		Service service3= new Service(Arrays.asList("isbn".split(" ")), 
-				Arrays.asList("discount".split(" ")),
-				"service 3");
-		
-		Service service4= new Service(Arrays.asList("price discount".split(" ")), 
-				Arrays.asList("location".split(" ")),
-				"service 4");
-		Service service5= new Service(Arrays.asList("paid price".split(" ")), 
-				Arrays.asList("location".split(" ")),
-				"service 5");
-		
-		List<Service> services = new ArrayList<Service>();
-		services.add(service3);
-		services.add(service5);
-		services.add(service4);
-		services.add(service2);
-		services.add(service1);
-//		System.out.println(solve(in, out, services));
-		
-	}
 	
-	public static boolean isCovered(List<String> s1, List<String> s2){
-		for (String s : s1)
-			if (!s2.contains(s))
-				return false;
-		return true;
-	}
-	
-	public static boolean isCovered2(List<String> s1, List<String> s2, OntModel m){
+	public static boolean isCovered(List<String> s1, List<String> s2, OntModel m){
 		for (String s : s1){
 			boolean flag = true;
 			OntClass oc = m.getOntClass(s);
